7246d1351c3bae3d2d2edd23a9c1ed7d31fc8fe3
devsuzy <suzy0529@gmail.com>
2022-06-23 11:18:43 +0900

    add2

--- main.js
+++ main.js
@@ -4,12 +4,51 @@ const CARROT_SIZE = 80;
 const CARROT_COUNT = 10;
 const BUG_COUNT = 10;
 
-// 1. Carrot, Bug Random Arrangement
-
+// 1
 const ground = document.querySelector(".ground");
 const groundRect = ground.getBoundingClientRect();
 
+// 2
+const gameStartBtn = document.querySelector(".play-btn");
+const gameTimer = document.querySelector(".timer");
+const gameScore = document.querySelector(".score");
+
+let started = false;
+let timer = 0;
+let score = undefined;
+
+// 2. Game Start
+
+gameStartBtn.addEventListener('click', () => {
+    if(started){
+        // stopGame();
+    } else{
+        startGame();
+    }
+    started = !started;
+})
+
+function startGame(){
+    initGame();
+    showStopGame();
+    showTimeAndScore();
+}
+
+function showStopGame(){
+    const icon = gameStartBtn.querySelector('.fa');
+    icon.classList.add('.fa-stop');
+    icon.classList.remove('.fa-play');
+}
+function showTimeAndScore(){
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+
+// 1. carrot, bug random Arrangement
+
 function initGame(){
+    ground.innerHTML = '';
+    gameScore.innerHTML = CARROT_COUNT;
     addItem('carrot',CARROT_COUNT,'img/carrot.png');
     addItem('bug',BUG_COUNT,'img/bug.png');
 }
@@ -35,6 +74,4 @@ function randomNumber(min, max){
     return Math.random() * (max - min) + min;
 }
 
-initGame();
 
-// 2. Game Start
